2716a5203ba806e645dcea76379e00c2d982c034
jossdev <josekandia42@gmail.com>
2023-08-28 11:14:29 +0200

    agrega botones

--- SistemaDeGestionHotel/Principal.Designer.cs
+++ SistemaDeGestionHotel/Principal.Designer.cs
@@ -28,18 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panelMenu = new Panel();
+            panelLogo = new Panel();
+            iconButtonHome = new FontAwesome.Sharp.IconButton();
+            iconButton2 = new FontAwesome.Sharp.IconButton();
+            iconButton3 = new FontAwesome.Sharp.IconButton();
+            iconButton4 = new FontAwesome.Sharp.IconButton();
+            panelMenu.SuspendLayout();
             SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.Indigo;
+            panelMenu.Controls.Add(iconButton4);
+            panelMenu.Controls.Add(iconButton3);
+            panelMenu.Controls.Add(iconButton2);
+            panelMenu.Controls.Add(iconButtonHome);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.ForeColor = SystemColors.ActiveCaptionText;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(220, 450);
+            panelMenu.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Padding = new Padding(20, 0, 10, 0);
+            panelLogo.Size = new Size(220, 140);
+            panelLogo.TabIndex = 0;
+            // 
+            // iconButtonHome
+            // 
+            iconButtonHome.Dock = DockStyle.Top;
+            iconButtonHome.FlatAppearance.BorderSize = 0;
+            iconButtonHome.FlatStyle = FlatStyle.Flat;
+            iconButtonHome.ForeColor = Color.White;
+            iconButtonHome.IconChar = FontAwesome.Sharp.IconChar.HomeLg;
+            iconButtonHome.IconColor = Color.White;
+            iconButtonHome.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButtonHome.IconSize = 38;
+            iconButtonHome.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButtonHome.Location = new Point(0, 140);
+            iconButtonHome.Name = "iconButtonHome";
+            iconButtonHome.Padding = new Padding(20, 0, 10, 0);
+            iconButtonHome.Size = new Size(220, 60);
+            iconButtonHome.TabIndex = 1;
+            iconButtonHome.Text = "Home";
+            iconButtonHome.TextAlign = ContentAlignment.MiddleLeft;
+            iconButtonHome.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButtonHome.UseVisualStyleBackColor = true;
+            // 
+            // iconButton2
+            // 
+            iconButton2.Dock = DockStyle.Top;
+            iconButton2.FlatAppearance.BorderSize = 0;
+            iconButton2.FlatStyle = FlatStyle.Flat;
+            iconButton2.ForeColor = Color.White;
+            iconButton2.IconChar = FontAwesome.Sharp.IconChar.HomeLg;
+            iconButton2.IconColor = Color.White;
+            iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton2.IconSize = 38;
+            iconButton2.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton2.Location = new Point(0, 200);
+            iconButton2.Name = "iconButton2";
+            iconButton2.Padding = new Padding(20, 0, 10, 0);
+            iconButton2.Size = new Size(220, 60);
+            iconButton2.TabIndex = 2;
+            iconButton2.Text = "iconButton2";
+            iconButton2.TextAlign = ContentAlignment.MiddleLeft;
+            iconButton2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton2.UseVisualStyleBackColor = true;
+            // 
+            // iconButton3
+            // 
+            iconButton3.Dock = DockStyle.Top;
+            iconButton3.FlatAppearance.BorderSize = 0;
+            iconButton3.FlatStyle = FlatStyle.Flat;
+            iconButton3.ForeColor = Color.White;
+            iconButton3.IconChar = FontAwesome.Sharp.IconChar.HomeLg;
+            iconButton3.IconColor = Color.White;
+            iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton3.IconSize = 38;
+            iconButton3.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton3.Location = new Point(0, 260);
+            iconButton3.Name = "iconButton3";
+            iconButton3.Padding = new Padding(20, 0, 10, 0);
+            iconButton3.Size = new Size(220, 60);
+            iconButton3.TabIndex = 3;
+            iconButton3.Text = "iconButton3";
+            iconButton3.TextAlign = ContentAlignment.MiddleLeft;
+            iconButton3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton3.UseVisualStyleBackColor = true;
+            // 
+            // iconButton4
+            // 
+            iconButton4.Dock = DockStyle.Top;
+            iconButton4.FlatAppearance.BorderSize = 0;
+            iconButton4.FlatStyle = FlatStyle.Flat;
+            iconButton4.ForeColor = Color.White;
+            iconButton4.IconChar = FontAwesome.Sharp.IconChar.HomeLg;
+            iconButton4.IconColor = Color.White;
+            iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton4.IconSize = 38;
+            iconButton4.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton4.Location = new Point(0, 320);
+            iconButton4.Name = "iconButton4";
+            iconButton4.Padding = new Padding(20, 0, 10, 0);
+            iconButton4.Size = new Size(220, 60);
+            iconButton4.TabIndex = 4;
+            iconButton4.Text = "iconButton4";
+            iconButton4.TextAlign = ContentAlignment.MiddleLeft;
+            iconButton4.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton4.UseVisualStyleBackColor = true;
             // 
             // Principal
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(panelMenu);
             Name = "Principal";
             Text = "Home";
+            panelMenu.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelMenu;
+        private FontAwesome.Sharp.IconButton iconButtonHome;
+        private Panel panelLogo;
+        private FontAwesome.Sharp.IconButton iconButton4;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton iconButton2;
     }
 }
