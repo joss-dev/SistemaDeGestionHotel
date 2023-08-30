e3c2f59bb3247fe4e80545363874fd6523f0bb1f
Joselo <joselo34@hotmail.com>
2023-08-30 10:11:31 -0300

    Agrega Login

--- SistemaDeGestionHotel/Login.Designer.cs
+++ SistemaDeGestionHotel/Login.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            LUsuario = new Label();
+            LPass = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // LUsuario
+            // 
+            LUsuario.AutoSize = true;
+            LUsuario.ForeColor = SystemColors.ControlText;
+            LUsuario.Location = new Point(85, 175);
+            LUsuario.Name = "LUsuario";
+            LUsuario.Size = new Size(47, 15);
+            LUsuario.TabIndex = 0;
+            LUsuario.Text = "Usuario";
+            // 
+            // LPass
+            // 
+            LPass.AutoSize = true;
+            LPass.Location = new Point(85, 236);
+            LPass.Name = "LPass";
+            LPass.Size = new Size(57, 15);
+            LPass.TabIndex = 1;
+            LPass.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(85, 193);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(85, 254);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(39, 79, 193);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(408, 130);
+            panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.logoHotel;
+            pictureBox1.Location = new Point(105, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(193, 124);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form2";
+            BackColor = Color.FromArgb(103, 209, 100);
+            ClientSize = new Size(408, 394);
+            Controls.Add(panel1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(LPass);
+            Controls.Add(LUsuario);
+            Name = "Login";
+            Text = "Login";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label LUsuario;
+        private Label LPass;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Panel panel1;
+        private PictureBox pictureBox1;
     }
 }
