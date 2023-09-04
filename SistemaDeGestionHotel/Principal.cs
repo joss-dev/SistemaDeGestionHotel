b3aa98ca0e6c326c32166939134b7292e47425e5
jossdev <josekandia42@gmail.com>
2023-09-04 12:19:10 +0200

    agrega metodo resaltado de boton

--- SistemaDeGestionHotel/Principal.cs
+++ SistemaDeGestionHotel/Principal.cs
@@ -8,15 +8,48 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FontAwesome.Sharp;
 
 namespace SistemaDeGestionHotel
 {
     public partial class Principal : Form
     {
+        //declaracion de icono y boton 
+        private IconButton currentBtn;
+        private Panel leftBorderBtn;
+
+
         public Principal()
         {
             InitializeComponent();
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(7, 60);
+            panelMenu.Controls.Add(leftBorderBtn);
         }
+
+        //metodo para resaltar el boton activo
+        private void ActivateButton(object senderBtn, Color color)
+        {
+            if(senderBtn != null)
+            {
+                //boton
+                currentBtn = (IconButton)senderBtn;
+                currentBtn.BackColor = Color.FromArgb(28, 121, 182);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign = ContentAlignment.MiddleRight;
+
+                //panel izquierdo
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0, currentBtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+            }
+        }
+
+        //metodo para desactivar el resaltado
 
         private void btnExit_Click(object sender, EventArgs e)
         {
