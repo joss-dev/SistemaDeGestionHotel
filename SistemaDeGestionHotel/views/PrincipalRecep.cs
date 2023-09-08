03a0d70169ea349b0ad2a2fc04303db50093c1b8
Jmjoss <josekandia42@gmail.com>
2023-09-08 00:17:46 -0300

    nose que paso

--- SistemaDeGestionHotel/views/PrincipalRecep.cs
+++ SistemaDeGestionHotel/views/PrincipalRecep.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using FontAwesome.Sharp;
+﻿using FontAwesome.Sharp;
 
 namespace SistemaDeGestionHotel
 {
