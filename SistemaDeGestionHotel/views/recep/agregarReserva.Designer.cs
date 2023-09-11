e2175126d0ed3cfd242661604ac4fae8c247d3d4
jossdev <josekandia42@gmail.com>
2023-09-11 13:12:02 +0200

    modifica textbox

--- SistemaDeGestionHotel/views/recep/agregarReserva.Designer.cs
+++ SistemaDeGestionHotel/views/recep/agregarReserva.Designer.cs
@@ -32,85 +32,92 @@
             textBox2 = new TextBox();
             textBox3 = new TextBox();
             textBox4 = new TextBox();
+            lApellido = new Label();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            label4 = new Label();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(134, 89);
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(299, 64);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(220, 23);
+            textBox1.Size = new Size(206, 23);
             textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(134, 160);
+            textBox2.Location = new Point(299, 128);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(220, 23);
+            textBox2.Size = new Size(206, 23);
             textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(134, 222);
+            textBox3.Location = new Point(299, 186);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(220, 23);
+            textBox3.Size = new Size(206, 23);
             textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(134, 299);
+            textBox4.Location = new Point(299, 249);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(220, 23);
+            textBox4.Size = new Size(206, 23);
             textBox4.TabIndex = 3;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // lApellido
+            // 
+            lApellido.AutoSize = true;
+            lApellido.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lApellido.Location = new Point(213, 67);
+            lApellido.Name = "lApellido";
+            lApellido.Size = new Size(67, 20);
+            lApellido.TabIndex = 4;
+            lApellido.Text = "Apellido";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(64, 97);
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(213, 131);
             label1.Name = "label1";
-            label1.Size = new Size(49, 15);
-            label1.TabIndex = 4;
-            label1.Text = "apellido";
+            label1.Size = new Size(71, 20);
+            label1.TabIndex = 5;
+            label1.Text = "Nombre ";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(64, 168);
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(234, 189);
             label2.Name = "label2";
-            label2.Size = new Size(49, 15);
-            label2.TabIndex = 5;
-            label2.Text = "apellido";
+            label2.Size = new Size(37, 20);
+            label2.TabIndex = 6;
+            label2.Text = "DNI";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(64, 225);
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(123, 252);
             label3.Name = "label3";
-            label3.Size = new Size(49, 15);
-            label3.TabIndex = 6;
-            label3.Text = "apellido";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(64, 299);
-            label4.Name = "label4";
-            label4.Size = new Size(49, 15);
-            label4.TabIndex = 7;
-            label4.Text = "apellido";
+            label3.Size = new Size(170, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Cantidad de huespedes";
             // 
             // agregarReserva
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(1, 110, 180);
             ClientSize = new Size(800, 450);
-            Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Controls.Add(lApellido);
             Controls.Add(textBox4);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
@@ -128,9 +135,9 @@
         private TextBox textBox2;
         private TextBox textBox3;
         private TextBox textBox4;
+        private Label lApellido;
         private Label label1;
         private Label label2;
         private Label label3;
-        private Label label4;
     }
 }
