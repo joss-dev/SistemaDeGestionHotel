978b9e113c7ff7f33cb966a3cabad587cafb28b8
jossdev <josekandia42@gmail.com>
2023-09-17 16:37:34 -0300

    agrega context

---
+++ SistemaDeGestionHotel/models/hotelParanaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeGestionHotel.models
+{
+    internal class HotelParanaContext : DbContext
+    {
+        public HotelParanaContext(DbContextOptions<HotelParanaContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
