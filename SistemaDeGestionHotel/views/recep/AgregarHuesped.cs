cd8b2cf20766db7153d362f1e6a6a2aec396ac3d
Joselo <joselo34@hotmail.com>
2023-09-22 20:19:05 -0300

    Crea form CRUD gestionPagos

---
+++ SistemaDeGestionHotel/views/recep/AgregarHuesped.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeGestionHotel.views.recep
+{
+    public partial class AgregarHuesped : Form
+    {
+        public AgregarHuesped()
+        {
+            InitializeComponent();
+
+            //establece la fecha minima para seleccionar
+            dateTimeIngreso.MinDate = DateTime.Today;
+            dateTimeSalida.MinDate = DateTime.Today;
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+
+            // Cerrar el formulario secundario
+            this.Close();
+        }
+
+        private void ValidacionNombre(object sender, KeyEventArgs e)
+        {
+            ValidacionTextBox.ValidarTextoConEspacios(TNombre, errorProviderNumero);
+        }
+
+        private void ValidacionApellido(object sender, KeyEventArgs e)
+        {
+            ValidacionTextBox.ValidarTextoConEspacios(TApellido, errorProviderNumero);
+        }
+
+        private void ValidacionDni(object sender, KeyEventArgs e)
+        {
+            ValidacionTextBox.ValidarSoloNumeros(TDni, errorProviderNumero);
+        }
+
+        private void ValidacionCantHuesped(object sender, KeyEventArgs e)
+        {
+            ValidacionTextBox.ValidarSoloNumeros(TCantidadHuespedes, errorProviderNumero);
+        }
+    }
+}
