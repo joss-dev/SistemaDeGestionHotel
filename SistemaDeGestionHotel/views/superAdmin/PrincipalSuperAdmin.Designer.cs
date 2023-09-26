f3692bd681569b66e427e4a0b898650697c504c0
jossdev <josekandia42@gmail.com>
2023-09-25 23:01:10 -0300

    agrega funciones principalSuperadmin

--- SistemaDeGestionHotel/views/superAdmin/PrincipalSuperAdmin.Designer.cs
+++ SistemaDeGestionHotel/views/superAdmin/PrincipalSuperAdmin.Designer.cs
@@ -28,12 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panelMenu = new Panel();
+            btnCerrarSesion = new Button();
+            btnHome = new FontAwesome.Sharp.IconButton();
+            panelLogo = new Panel();
+            pictureLogo = new PictureBox();
+            btnMinimizar = new FontAwesome.Sharp.IconButton();
+            btnExit = new FontAwesome.Sharp.IconButton();
+            btnPantallaCompleta = new FontAwesome.Sharp.IconButton();
+            panelMenu.SuspendLayout();
+            panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureLogo).BeginInit();
+            SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(39, 79, 193);
+            panelMenu.Controls.Add(btnCerrarSesion);
+            panelMenu.Controls.Add(btnHome);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.ForeColor = SystemColors.ActiveCaptionText;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(220, 544);
+            panelMenu.TabIndex = 1;
+            // 
+            // btnCerrarSesion
+            // 
+            btnCerrarSesion.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnCerrarSesion.BackColor = Color.FromArgb(40, 40, 40);
+            btnCerrarSesion.FlatAppearance.BorderSize = 0;
+            btnCerrarSesion.FlatAppearance.MouseDownBackColor = Color.FromArgb(28, 28, 28);
+            btnCerrarSesion.FlatAppearance.MouseOverBackColor = Color.FromArgb(64, 64, 64);
+            btnCerrarSesion.FlatStyle = FlatStyle.Flat;
+            btnCerrarSesion.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnCerrarSesion.ForeColor = SystemColors.GrayText;
+            btnCerrarSesion.Location = new Point(39, 947);
+            btnCerrarSesion.Name = "btnCerrarSesion";
+            btnCerrarSesion.Size = new Size(134, 39);
+            btnCerrarSesion.TabIndex = 5;
+            btnCerrarSesion.Text = "Cerrar Sesión";
+            btnCerrarSesion.UseVisualStyleBackColor = false;
+            // 
+            // btnHome
+            // 
+            btnHome.Cursor = Cursors.Hand;
+            btnHome.Dock = DockStyle.Top;
+            btnHome.FlatAppearance.BorderSize = 0;
+            btnHome.FlatAppearance.MouseDownBackColor = Color.Gainsboro;
+            btnHome.FlatAppearance.MouseOverBackColor = Color.Gray;
+            btnHome.FlatStyle = FlatStyle.Flat;
+            btnHome.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnHome.ForeColor = Color.White;
+            btnHome.IconChar = FontAwesome.Sharp.IconChar.HomeLg;
+            btnHome.IconColor = Color.FromArgb(15, 15, 15);
+            btnHome.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnHome.IconSize = 38;
+            btnHome.ImageAlign = ContentAlignment.MiddleLeft;
+            btnHome.Location = new Point(0, 140);
+            btnHome.Name = "btnHome";
+            btnHome.Padding = new Padding(20, 0, 10, 0);
+            btnHome.Size = new Size(220, 60);
+            btnHome.TabIndex = 1;
+            btnHome.Text = "Home";
+            btnHome.TextAlign = ContentAlignment.MiddleLeft;
+            btnHome.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnHome.UseVisualStyleBackColor = true;
+            // 
+            // panelLogo
+            // 
+            panelLogo.Controls.Add(pictureLogo);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Padding = new Padding(20, 0, 10, 0);
+            panelLogo.Size = new Size(220, 140);
+            panelLogo.TabIndex = 0;
+            // 
+            // pictureLogo
+            // 
+            pictureLogo.BackColor = Color.FromArgb(39, 79, 193);
+            pictureLogo.Image = Properties.Resources.logoHotel;
+            pictureLogo.Location = new Point(0, 0);
+            pictureLogo.Name = "pictureLogo";
+            pictureLogo.Size = new Size(220, 140);
+            pictureLogo.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureLogo.TabIndex = 0;
+            pictureLogo.TabStop = false;
+            // 
+            // btnMinimizar
+            // 
+            btnMinimizar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMinimizar.FlatAppearance.BorderSize = 0;
+            btnMinimizar.FlatStyle = FlatStyle.Flat;
+            btnMinimizar.IconChar = FontAwesome.Sharp.IconChar.WindowMinimize;
+            btnMinimizar.IconColor = Color.Black;
+            btnMinimizar.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            btnMinimizar.IconSize = 20;
+            btnMinimizar.ImageAlign = ContentAlignment.BottomRight;
+            btnMinimizar.Location = new Point(790, 0);
+            btnMinimizar.Name = "btnMinimizar";
+            btnMinimizar.Size = new Size(29, 26);
+            btnMinimizar.TabIndex = 6;
+            btnMinimizar.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnExit.FlatAppearance.BorderSize = 0;
+            btnExit.FlatStyle = FlatStyle.Flat;
+            btnExit.IconChar = FontAwesome.Sharp.IconChar.X;
+            btnExit.IconColor = Color.Black;
+            btnExit.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            btnExit.IconSize = 20;
+            btnExit.Location = new Point(861, 0);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(33, 30);
+            btnExit.TabIndex = 5;
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnPantallaCompleta
+            // 
+            btnPantallaCompleta.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnPantallaCompleta.FlatAppearance.BorderSize = 0;
+            btnPantallaCompleta.FlatStyle = FlatStyle.Flat;
+            btnPantallaCompleta.IconChar = FontAwesome.Sharp.IconChar.Square;
+            btnPantallaCompleta.IconColor = Color.Black;
+            btnPantallaCompleta.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            btnPantallaCompleta.IconSize = 20;
+            btnPantallaCompleta.Location = new Point(825, 0);
+            btnPantallaCompleta.Name = "btnPantallaCompleta";
+            btnPantallaCompleta.Size = new Size(33, 26);
+            btnPantallaCompleta.TabIndex = 7;
+            btnPantallaCompleta.UseVisualStyleBackColor = true;
+            // 
+            // PrincipalSuperAdmin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(893, 544);
+            Controls.Add(btnMinimizar);
+            Controls.Add(btnExit);
+            Controls.Add(btnPantallaCompleta);
+            Controls.Add(panelMenu);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "PrincipalSuperAdmin";
+            Text = "Form1";
+            panelMenu.ResumeLayout(false);
+            panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureLogo).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelMenu;
+        private Button btnCerrarSesion;
+        private FontAwesome.Sharp.IconButton btnHome;
+        private Panel panelLogo;
+        private PictureBox pictureLogo;
+        private FontAwesome.Sharp.IconButton btnMinimizar;
+        private FontAwesome.Sharp.IconButton btnExit;
+        private FontAwesome.Sharp.IconButton btnPantallaCompleta;
     }
 }
