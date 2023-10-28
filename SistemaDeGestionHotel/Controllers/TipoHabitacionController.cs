c546d41b575b684a1f7b4cf3577bf00e9210102e
jossdev <josekandia42@gmail.com>
2023-10-28 20:08:05 -0300

    agrega tipo de habitacion  controller

---
+++ SistemaDeGestionHotel/Controllers/TipoHabitacionController.cs
@@ -0,0 +1,48 @@
+﻿using SistemaDeGestionHotel.Datos;
+using SistemaDeGestionHotel.NEntidades;
+
+namespace SistemaDeGestionHotel.Controllers
+{
+    public class TipoHabitacionController
+    {
+        DTipoHabitacion d_tipoHabitacion = new DTipoHabitacion();
+
+        public bool AgregarTipoHabitacion(string nombTipo)
+        {
+            TipoHabitacion tipoHabitacion = new TipoHabitacion()
+            {
+                NombTipo = nombTipo
+            };
+
+            return d_tipoHabitacion.AgregarTipoHabitacion(tipoHabitacion);
+        }
+
+        public List<TipoHabitacion> GetTiposHabitacion()
+        {
+            return d_tipoHabitacion.GetTiposHabitacion();
+        }
+
+        public TipoHabitacion GetTipoHabitacionByID(int id)
+        {
+            return d_tipoHabitacion.GetTipoHabitacionByID(id);
+        }
+
+        public bool EditarTipoHabitacion(int idTipo, string nombTipo)
+        {
+            TipoHabitacion tipoExistente = d_tipoHabitacion.GetTipoHabitacionByID(idTipo);
+
+            if (tipoExistente == null)
+            {
+                // El tipo de habitación no existe, por lo tanto no se puede editar
+                return false;
+            }
+
+            // El tipo de habitación existe, actualiza sus propiedades
+            tipoExistente.NombTipo = nombTipo;
+
+            d_tipoHabitacion.GuardarCambios();
+
+            return true;
+        }
+    }
+}
---
+++ SistemaDeGestionHotel/Datos/DTipoHabitacion.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeGestionHotel.NEntidades;
+
+namespace SistemaDeGestionHotel.Datos
+{
+    internal class DTipoHabitacion
+    {
+        private HotelParanaContext dbHotelParana;
+
+        public DTipoHabitacion()
+        {
+            dbHotelParana = new HotelParanaContext();
+        }
+
+        public bool AgregarTipoHabitacion(TipoHabitacion tipo)
+        {
+            bool result = true;
+            try
+            {
+                // Intenta insertar el nuevo tipo de habitación en la base de datos
+                dbHotelParana.Add(tipo);
+                dbHotelParana.SaveChanges();
+                return result;
+            }
+            catch (DbUpdateException ex)
+            {
+                result = false;
+                return result;
+            }
+        }
+
+        public List<TipoHabitacion> GetTiposHabitacion()
+        {
+            return dbHotelParana.TipoHabitacions.ToList();
+        }
+
+        public TipoHabitacion GetTipoHabitacionByID(int id)
+        {
+            return dbHotelParana.TipoHabitacions
+                                 .FirstOrDefault(t => t.IdTipoHab == id);
+        }
+
+        public void GuardarCambios()
+        {
+            dbHotelParana.SaveChanges();
+        }
+    }
+}
