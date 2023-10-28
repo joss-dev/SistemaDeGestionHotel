bd181492b5525df3c304a580f5bf786443e7ff8e
jossdev <josekandia42@gmail.com>
2023-10-28 14:03:20 -0300

    agrega controller y capa de datos de habitacion

---
+++ SistemaDeGestionHotel/Controllers/HabitacionController.cs
@@ -0,0 +1,76 @@
+﻿using SistemaDeGestionHotel.Datos;
+using SistemaDeGestionHotel.NEntidades;
+
+namespace SistemaDeGestionHotel.Controllers
+{
+    public class HabitacionController
+    {
+        DHabitacion d_habitacion = new DHabitacion();
+
+        public bool AgregarHabitacion(int nrodehabitacion, int cantidaddecamas, float precio, int id_estado, int id_tipohab, int id_piso)
+        {
+            Habitacion habitacion = new Habitacion()
+            {
+                NroHabitacion = nrodehabitacion,
+                CantidadCamas = cantidaddecamas,
+                Precio = precio,
+                IdEstado = id_estado,
+                IdTipoHab = id_tipohab,
+                IdPiso = id_piso
+            };
+
+            return d_habitacion.AgregarHabitacion(habitacion);
+        }
+
+        public List<Habitacion> GetHabitaciones()
+        {
+            return d_habitacion.GetHabitaciones();
+        }
+
+        public Habitacion GetHabitacionByNro(int nrodehabitacion)
+        {
+            return d_habitacion.GetHabitacionByNro(nrodehabitacion);
+        }
+
+        public bool EditarHabitacion(int idHabitacion, int nrodehabitacion, int cantidaddecamas, float precio, int id_estado, int id_tipohab, int id_piso)
+        {
+            Habitacion habitacionExistente = d_habitacion.GetHabitacionByNro(idHabitacion);
+
+            if (habitacionExistente == null)
+            {
+                // La habitación no existe, por lo tanto no se puede editar
+                return false;
+            }
+
+            // La habitación existe, actualiza sus propiedades
+            habitacionExistente.NroHabitacion = nrodehabitacion;
+            habitacionExistente.CantidadCamas = cantidaddecamas;
+            habitacionExistente.Precio = precio;
+            habitacionExistente.IdEstado = id_estado;
+            habitacionExistente.IdTipoHab = id_tipohab;
+            habitacionExistente.IdPiso = id_piso;
+
+            d_habitacion.GuardarCambios();
+
+            return true;
+        }
+
+        public bool BajaHabitacion(int idHabitacion)
+        {
+            Habitacion habitacionExistente = d_habitacion.GetHabitacionByNro(idHabitacion);
+
+            if (habitacionExistente == null)
+            {
+                // La habitación no existe, por lo tanto no se puede editar
+                return false;
+            }
+
+            habitacionExistente.IdEstadoNavigation.IdEstado = 4;
+
+            d_habitacion.GuardarCambios();
+
+            return true;
+        }
+    }
+}
+
---
+++ SistemaDeGestionHotel/Datos/DHabitacion.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeGestionHotel.NEntidades;
+
+namespace SistemaDeGestionHotel.Datos
+{
+    internal class DHabitacion
+    {
+        private HotelParanaContext dbHotelParana;
+
+        public DHabitacion()
+        {
+            dbHotelParana = new HotelParanaContext();
+        }
+
+        public bool AgregarHabitacion(Habitacion habitacion)
+        {
+            bool result = true;
+            try
+            {
+                // Intenta insertar la nueva habitation en la base de datos
+                dbHotelParana.Add(habitacion);
+                dbHotelParana.SaveChanges();
+                return result;
+            }
+            catch (DbUpdateException ex)
+            {
+                result = false;
+                return result;
+            }
+        }
+
+        public List<Habitacion> GetHabitaciones()
+        {
+            return dbHotelParana.Habitacions.ToList();
+        }
+
+        public Habitacion GetHabitacionByNro(int nrodehabitation)
+        {
+            return dbHotelParana.Habitacions
+                                 .FirstOrDefault(h => h.NroHabitacion == nrodehabitation);
+        }
+
+
+        public void GuardarCambios()
+        {
+            dbHotelParana.SaveChanges();
+        }
+    }
+}
