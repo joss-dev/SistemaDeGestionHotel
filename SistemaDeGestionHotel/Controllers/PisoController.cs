0d8601fe822be3fa5b5c4d9dce7d94c2be42ca10
jossdev <josekandia42@gmail.com>
2023-10-28 20:27:28 -0300

    agrega piso controller

--- SistemaDeGestionHotel/Controllers/PisoController.cs
+++ SistemaDeGestionHotel/Controllers/PisoController.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SistemaDeGestionHotel.Datos;
+using SistemaDeGestionHotel.NEntidades;
 
 namespace SistemaDeGestionHotel.Controllers
 {
-    internal class PisoController
+    public class PisoController
     {
+        DPiso d_piso = new DPiso();
+
+        public bool AgregarPiso(int nroPiso)
+        {
+            Piso piso = new Piso()
+            {
+                NroPiso = nroPiso
+            };
+
+            return d_piso.AgregarPiso(piso);
+        }
+
+        public List<Piso> GetPisos()
+        {
+            return d_piso.GetPisos();
+        }
+
+        public Piso GetPisoByID(int id)
+        {
+            return d_piso.GetPisoByID(id);
+        }
+
+        public bool EditarPiso(int idPiso, int nroPiso)
+        {
+            Piso pisoExistente = d_piso.GetPisoByID(idPiso);
+
+            if (pisoExistente == null)
+            {
+                // El piso no existe, por lo tanto no se puede editar
+                return false;
+            }
+
+            // El piso existe, actualiza sus propiedades
+            pisoExistente.NroPiso = nroPiso;
+
+            d_piso.GuardarCambios();
+
+            return true;
+        }
     }
 }
--- SistemaDeGestionHotel/Datos/DPiso.cs
+++ SistemaDeGestionHotel/Datos/DPiso.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeGestionHotel.NEntidades;
 
 namespace SistemaDeGestionHotel.Datos
 {
     internal class DPiso
     {
+        private HotelParanaContext dbHotelParana;
+
+        public DPiso()
+        {
+            dbHotelParana = new HotelParanaContext();
+        }
+
+        public bool AgregarPiso(Piso piso)
+        {
+            bool result = true;
+            try
+            {
+                // Intenta insertar el nuevo piso en la base de datos
+                dbHotelParana.Add(piso);
+                dbHotelParana.SaveChanges();
+                return result;
+            }
+            catch (DbUpdateException ex)
+            {
+                result = false;
+                return result;
+            }
+        }
+
+        public List<Piso> GetPisos()
+        {
+            return dbHotelParana.Pisos.ToList();
+        }
+
+        public Piso GetPisoByID(int id)
+        {
+            return dbHotelParana.Pisos
+                                 .FirstOrDefault(p => p.IdPiso == id);
+        }
+
+        public void GuardarCambios()
+        {
+            dbHotelParana.SaveChanges();
+        }
     }
 }
