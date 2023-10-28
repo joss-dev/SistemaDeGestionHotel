a4000b69f18b7d9e280edf12e191bb0220e06d3a
jossdev <josekandia42@gmail.com>
2023-10-28 20:24:35 -0300

    agrega estado de habitacion controller

---
+++ SistemaDeGestionHotel/Controllers/EstadoHabitacionController.cs
@@ -0,0 +1,48 @@
+﻿using SistemaDeGestionHotel.Datos;
+using SistemaDeGestionHotel.NEntidades;
+
+namespace SistemaDeGestionHotel.Controllers
+{
+    public class EstadoHabitacionController
+    {
+        DEstadoHabitacion d_estadoHabitacion = new DEstadoHabitacion();
+
+        public bool AgregarEstadoHabitacion(string nombEstado)
+        {
+            EstadoHabitacion estadoHabitacion = new EstadoHabitacion()
+            {
+                NombEstado = nombEstado
+            };
+
+            return d_estadoHabitacion.AgregarEstadoHabitacion(estadoHabitacion);
+        }
+
+        public List<EstadoHabitacion> GetEstadosHabitaciones()
+        {
+            return d_estadoHabitacion.GetEstadosHabitaciones();
+        }
+
+        public EstadoHabitacion GetEstadoHabitacionByID(int id)
+        {
+            return d_estadoHabitacion.GetEstadoHabitacionByID(id);
+        }
+
+        public bool EditarEstadoHabitacion(int idEstado, string nombEstado)
+        {
+            EstadoHabitacion estadoExistente = d_estadoHabitacion.GetEstadoHabitacionByID(idEstado);
+
+            if (estadoExistente == null)
+            {
+                // El estado de la habitación no existe, por lo tanto no se puede editar
+                return false;
+            }
+
+            // El estado de la habitación existe, actualiza sus propiedades
+            estadoExistente.NombEstado = nombEstado;
+
+            d_estadoHabitacion.GuardarCambios();
+
+            return true;
+        }
+    }
+}
---
+++ SistemaDeGestionHotel/Datos/DEstadoHabitacion.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeGestionHotel.NEntidades;
+
+namespace SistemaDeGestionHotel.Datos
+{
+    internal class DEstadoHabitacion
+    {
+        private HotelParanaContext dbHotelParana;
+
+        public DEstadoHabitacion()
+        {
+            dbHotelParana = new HotelParanaContext();
+        }
+
+        public bool AgregarEstadoHabitacion(EstadoHabitacion estado)
+        {
+            bool result = true;
+            try
+            {
+                // Intenta insertar el nuevo estado de habitación en la base de datos
+                dbHotelParana.Add(estado);
+                dbHotelParana.SaveChanges();
+                return result;
+            }
+            catch (DbUpdateException ex)
+            {
+                result = false;
+                return result;
+            }
+        }
+
+        public List<EstadoHabitacion> GetEstadosHabitaciones()
+        {
+            return dbHotelParana.EstadoHabitacions.ToList();
+        }
+
+        public EstadoHabitacion GetEstadoHabitacionByID(int id)
+        {
+            return dbHotelParana.EstadoHabitacions
+                                 .FirstOrDefault(e => e.IdEstado == id);
+        }
+
+        public void GuardarCambios()
+        {
+            dbHotelParana.SaveChanges();
+        }
+    }
+}
