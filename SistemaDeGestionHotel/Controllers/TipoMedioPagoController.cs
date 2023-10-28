a667d0883cc6357698d5b61b98e0114297020bae
jossdev <josekandia42@gmail.com>
2023-10-28 20:17:34 -0300

    agrega tipomedio de pago controller

---
+++ SistemaDeGestionHotel/Controllers/TipoMedioPagoController.cs
@@ -0,0 +1,48 @@
+﻿using SistemaDeGestionHotel.Datos;
+using SistemaDeGestionHotel.NEntidades;
+
+namespace SistemaDeGestionHotel.Controllers
+{
+    public class TipoMedioPagoController
+    {
+        DTipoMedioPago d_tipoMedioPago = new DTipoMedioPago();
+
+        public bool AgregarTipoMedioPago(string nombMedioPago)
+        {
+            TipoMedioPago tipoMedioPago = new TipoMedioPago()
+            {
+                NombMedioPago = nombMedioPago
+            };
+
+            return d_tipoMedioPago.AgregarTipoMedioPago(tipoMedioPago);
+        }
+
+        public List<TipoMedioPago> GetTiposMediosPagos()
+        {
+            return d_tipoMedioPago.GetTiposMediosPagos();
+        }
+
+        public TipoMedioPago GetTipoMedioPagoByID(int id)
+        {
+            return d_tipoMedioPago.GetTipoMedioPagoByID(id);
+        }
+
+        public bool EditarTipoMedioPago(int idTipo, string nombMedioPago)
+        {
+            TipoMedioPago tipoExistente = d_tipoMedioPago.GetTipoMedioPagoByID(idTipo);
+
+            if (tipoExistente == null)
+            {
+                // El tipo de medio de pago no existe, por lo tanto no se puede editar
+                return false;
+            }
+
+            // El tipo de medio de pago existe, actualiza sus propiedades
+            tipoExistente.NombMedioPago = nombMedioPago;
+
+            d_tipoMedioPago.GuardarCambios();
+
+            return true;
+        }
+    }
+}
---
+++ SistemaDeGestionHotel/Datos/DTipoMedioPago.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeGestionHotel.NEntidades;
+
+namespace SistemaDeGestionHotel.Datos
+{
+    internal class DTipoMedioPago
+    {
+        private HotelParanaContext dbHotelParana;
+
+        public DTipoMedioPago()
+        {
+            dbHotelParana = new HotelParanaContext();
+        }
+
+        public bool AgregarTipoMedioPago(TipoMedioPago tipo)
+        {
+            bool result = true;
+            try
+            {
+                // Intenta insertar el nuevo tipo de medio de pago en la base de datos
+                dbHotelParana.Add(tipo);
+                dbHotelParana.SaveChanges();
+                return result;
+            }
+            catch (DbUpdateException ex)
+            {
+                result = false;
+                return result;
+            }
+        }
+
+        public List<TipoMedioPago> GetTiposMediosPagos()
+        {
+            return dbHotelParana.TipoMedioPagos.ToList();
+        }
+
+        public TipoMedioPago GetTipoMedioPagoByID(int id)
+        {
+            return dbHotelParana.TipoMedioPagos
+                                 .FirstOrDefault(t => t.IdTipoMedioPago == id);
+        }
+
+        public void GuardarCambios()
+        {
+            dbHotelParana.SaveChanges();
+        }
+    }
+}
